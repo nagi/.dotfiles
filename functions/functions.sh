10bbc8acd18f118cb8bc9a92639f164f440bbefe
Andrew Nagi <andrew.nagi@gmail.com>
2016-09-08 12:57:58 +0100

    Tidy up git function

--- functions/functions.sh
+++ functions/functions.sh
@@ -13,3 +13,5 @@ function remove_lines_from() { grep -F -x -v -f $2 $1; }
 function mkcd() { mkdir $1 && cd $1; }
 
 function ed209 { git status --porcelain | grep -v '^D' | grep '\.rb$' | grep -v 'db/schema.rb' | cut -d ' ' -f 3 | xargs bundle exec rubocop }
+
+function git_clean_up { git branch --merged | grep -v "\*" | grep -v master | grep -v dev | xargs -n 1 git branch -d }

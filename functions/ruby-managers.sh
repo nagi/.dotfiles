1562ecaf1acf0eaf64dc573db8226bf9329a0f9b
Andrew Nagi <andrew.nagi@gmail.com>
2016-10-07 16:01:47 +0100

    Ruby stuff

--- functions/ruby-managers.sh
+++ functions/ruby-managers.sh
@@ -3,8 +3,13 @@
 [[ -x "$HOME/.rvm/scripts/rvm" ]] && PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
 
 # rbenv
-which rbenv > /dev/null && eval "$(rbenv init -)" && rbenv global > /dev/null
+if [ -d "$HOME/.rbenv/bin" ]; then
+  export PATH="$HOME/.rbenv/bin:$PATH"
+  eval "$(rbenv init -)"
+  rbenv global > /dev/null
+fi
 
 # chruby
 [[ -s /usr/local/share/chruby/chruby.sh ]] && source /usr/local/share/chruby/chruby.sh
+[[ -s /usr/local/share/chruby/auto.sh ]] && source /usr/local/share/chruby/auto.sh
 which chruby > /dev/null && chruby 2.2.2

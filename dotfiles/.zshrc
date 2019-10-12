45f858215ff6d730c7e6a7ca242d489cbef2d04d
Andrew Nagi <andrew.nagi@gmail.com>
2019-10-12 18:37:11 +0100

    Move stuff to zshenv for non-interactive shells

--- dotfiles/.zshrc
+++ dotfiles/.zshrc
@@ -3,17 +3,11 @@ ZSH_THEME="robbyrussell"
 DISABLE_AUTO_UPDATE="true"
 plugins=(brew capistrano coffee colorize cp docker git git-flow-avh github lein node npm osx pip postgres rake ruby rvm tmux tmuxinator vi-mode vagrant vagrant-prompt)
 
-source $ZSH/oh-my-zsh.sh
+autoload edit-command-line; zle -N edit-command-line
 
 bindkey "^R" history-incremental-search-backward
-
-autoload edit-command-line; zle -N edit-command-line
 bindkey -M vicmd v edit-command-line
 
 source $HOME/.dotfiles/functions/aliases.sh
 source $HOME/.dotfiles/functions/functions.sh
-source $HOME/.dotfiles/functions/system-specific.sh
-source $HOME/.dotfiles/functions/ruby-managers.sh
-
-# Confidential stuff
-[[ -s "$HOME/.dotfiles/functions/secrets.sh" ]] && source "$HOME/.dotfiles/functions/secrets.sh"
+source $ZSH/oh-my-zsh.sh
--- functions/system-specific.sh
+++ functions/system-specific.sh
@@ -1,18 +1,36 @@
 if [ `uname` = Darwin ]
 then
-  # load iTerm2 tab color helpers
-  [[ -s ~/.dotfiles/functions/iterm2-helpers.sh ]] && source ~/.dotfiles/functions/iterm2-helpers.sh
+    # load iTerm2 tab color helpers
+    [[ -s ~/.dotfiles/functions/iterm2-helpers.sh ]] && source ~/.dotfiles/functions/iterm2-helpers.sh
+
+    # Prefer homebrew applications.
+    export PATH=/usr/local/bin:/usr/local/sbin:/usr/local/games:/usr/local/mysql/bin:$PATH
+
+    export PATH="/usr/local/opt/mongodb@3.0/bin:$PATH"
+    export PATH="/usr/local/opt/postgresql@9.4/bin:$PATH"
+    export PATH="/usr/local/opt/postgresql@9.4/bin:$PATH"
+    # Put vagrant boxes on pendrive
+    export VAGRANT_HOME='/Volumes/Quicker/vagrant.d'
+
+    export ANDROID_HOME=/Volumes/Quicker/android-sdk-macosx
 else
-  # Run ssh-agent: https://wiki.archlinux.org/index.php/SSH_keys#ssh-agent
-  # eval `keychain --eval --agents ssh id_rsa --quiet`
+    # Run ssh-agent: https://wiki.archlinux.org/index.php/SSH_keys#ssh-agent
+    [[ -s /etc/arch-release ]] && eval `keychain --eval --agents ssh id_rsa --quiet`
 
-  # Debain / Ubuntu / Mint command not found
-  if [ -f /etc/zsh_command_not_found ]
-  then
-    . /etc/zsh_command_not_found
-  fi
+    # Debain / Ubuntu / Mint command not found
+    if [ -f /etc/zsh_command_not_found ]
+    then
+        . /etc/zsh_command_not_found
+    fi
 
-  # sane clipboard
-  alias pbcopy='xclip -selection clipboard'
-  alias pbpaste='xclip -selection clipboard -o'
+    # sane clipboard
+    alias pbcopy='xclip -selection clipboard'
+    alias pbpaste='xclip -selection clipboard -o'
+
+    # Datomic
+    export PATH=$PATH:/usr/local/src/datomic-pro-0.9.5561/bin
+    export PATH=$PATH:/usr/local/src/datomic-free-0.9.5394/bin
+
+    # Android studio
+    export PATH="/opt/android-studio/bin:$PATH"
 fi
